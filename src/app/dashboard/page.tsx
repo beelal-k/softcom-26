73bf665310808abac9c184c416893c8f2acce6b8
Ahmad-tecnofuzion <mahmad.ss2103@gmail.com>
2025-12-20 08:37:34 +0500

    yesyeysyesyeyyeys

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
 import { redirect } from 'next/navigation';
 
-export default async function Dashboard() {
-  // const { userId } = await auth();
-  // if (!userId) {
-  //   return redirect('/auth/sign-in');
-  // } else {
-  //   redirect('/dashboard/overview');
-  // }
+export default function Dashboard() {
+  redirect('/dashboard/overview');
 }
